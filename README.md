7599c9c047ffdd06f4a1049071b034bfebca4765
Matthew Sanetra <matthewsanetra@gmail.com>
2019-10-23 16:27:03 +0100

    Initial functionality

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 A fast in-browser proxy that can replace any expression/key words from HTML.
 
 I initially won't be working on adding CSS & JS support, but once I am able to modify links from HTML source it will be trivial
+
+
+### TODO
+[ ] Nginx/caddy (Only passthrough /proxy)
+[ ] Make an index site
+[ ] Allow users to enter their own word that censored items get replaced with
+[ ] Replace urls in the html with a proxy url
+[ ] Containerize
---
+++ rot13.go
@@ -0,0 +1,34 @@
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the ROT13_LICENSE file.
+
+package main
+
+import (
+	"io"
+)
+
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	for i := 0; i < n; i++ {
+		p[i] = rot13(p[i])
+	}
+	return
+}
