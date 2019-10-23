f3d13a415b9b6a72059a647cab16c1a30e734980
Matthew Sanetra <matthewsanetra@gmail.com>
2019-10-23 21:41:38 +0100

    Redirect embedded links to proxy equivalent

---
+++ censor.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func censorBody(body []byte, censorRegex *regexp.Regexp, originalUrl, originalCensor string) (string, error) {
+	reader := bytes.NewReader(body)
+
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return "", err
+	}
+
+	var errors = make([]error, 0)
+
+	doc.Find("*[href],*[src],form[action]").Each(func(i int, s *goquery.Selection) {
+		attr_name := "href"
+		link, exists := s.Attr("href")
+		if !exists {
+			link, exists = s.Attr("src")
+			if !exists {
+				link, exists = s.Attr("action")
+				if !exists {
+					return
+				} else {
+					attr_name = "action"
+				}
+			} else {
+				attr_name = "src"
+			}
+		}
+
+		link, err = url.QueryUnescape(link)
+		if err != nil {
+			errors = append(errors, err)
+			return
+		}
+
+		var rot13_buf strings.Builder
+		c := strings.NewReader(link)
+		r := rot13Reader{c}
+
+		_, err = io.Copy(&rot13_buf, r)
+		if err != nil {
+			errors = append(errors, err)
+			return
+		}
+
+		rot13_url, err := url.Parse(rot13_buf.String())
+		if err != nil {
+			errors = append(errors, err)
+			return
+		}
+
+		originalUrlParsed, err := url.Parse(originalUrl)
+		if err != nil {
+			errors = append(errors, err)
+			return
+		}
+
+		if rot13_url.Host == "" {
+			rot13_url.Host = originalUrlParsed.Host
+		}
+		if rot13_url.Scheme == "" {
+			rot13_url.Scheme = originalUrlParsed.Scheme
+		}
+
+		newLink := "//127.0.0.1:8080/proxy?u=" + url.QueryEscape(rot13_url.String()) + "&c=" + originalCensor
+
+		s.SetAttr(attr_name, newLink)
+	})
+
+	if len(errors) != 0 {
+		return "", errors[0]
+	}
+
+	new_body, err := doc.Html()
+	if err != nil {
+		return "", err
+	}
+
+	new_body = censorRegex.ReplaceAllString(new_body, "censored")
+
+	return new_body, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/matthewsanetra/filterproxy
 
 go 1.13
 
-require github.com/valyala/fasthttp v1.5.0
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/valyala/fasthttp v1.5.0
+)
